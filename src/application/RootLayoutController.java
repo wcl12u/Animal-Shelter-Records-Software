43dd6b00b0ff2c9b4a05f58b2a3f2db52f56b447
wcl12u <william@ngcci.com>
2018-02-14 14:04:11 -0500

    Start of Controller

--- src/application/RootLayoutController.java
+++ src/application/RootLayoutController.java
@@ -1,8 +1,24 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.image.ImageView;
+
 public class RootLayoutController {
 	
 	private Main mainApp;
+	
+	@FXML
+	private Tab basicView;
+	@FXML
+	private Tab vetView;
+	@FXML
+	private Tab ownerView;
+	@FXML
+	private Tab manageView;
+	@FXML
+	private ImageView imgAnimalImage;
+	
 	
 	public void initialize() {
 		
